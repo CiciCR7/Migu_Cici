5779e7a3f9fdcc69a83f284b2767b4db6535f60d
Cici <98153392+CiciCR7@users.noreply.github.com>
2024-09-22 15:04:07 +0800

    Update LLM_api_Prompt.py

--- LLM_api_Prompt.py
+++ LLM_api_Prompt.py
@@ -1,12 +1,12 @@
 from openai import OpenAI
 # Set OpenAI's API key and API base to use vLLM's API server.
-openai_api_key = "EMPTY"
-openai_api_base = "http://192.168.0.11:7090/v1"
+openai_api_key = "EMPTY"  # 此处通常是OpenAI API密钥，该处设为空时，一般可能是调用本地部署的api
+openai_api_base = "http://192.168.0.11:7090/v1"，# 设置 API 基础 URL。这里的地址 http://192.168.0.11:7090/v1 指向本地的 vLLM（大语言模型） API 服务器，而不是 OpenAI 官方的 API。
 
 client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
-)
+)# 创建 OpenAI 类的一个实例 client，通过 api_key 和 base_url 参数指定使用的 API 密钥和基础 URL。client 是后续与 API 交互的对象。
 
 
 fp = open('jieri.txt', 'w')
@@ -15,7 +15,8 @@ loop_times = 15
 topic_list = ["节日装饰", "传统习俗", "家庭团聚", "节日氛围", "浪漫场景", "情侣互动", "爱情表达"]
 for topic in topic_list:
     for i in range(loop_times):
-        chat_response = client.chat.completions.create(
+        chat_response = client.chat.completions.create( # 
+            #使用 client 对象的 chat.completions.create() 方法来调用大语言模型生成内容。该方法负责向 API 发出请求，并获取聊天或完成任务的响应。
             model="Qwen2-57b-A14-Instruct",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
@@ -24,6 +25,6 @@ for topic in topic_list:
         )
         print("Chat response:", chat_response)
         fp.write(chat_response.choices[0].message.content.replace('\n\n', '\n'))
-        fp.flush()
+        fp.flush()# ：强制将缓冲区的数据写入文件，确保生成内容即时写入而不是等待缓冲区满时才写入。
 
 fp.close()
